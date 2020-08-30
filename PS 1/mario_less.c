040b1f54d69a3bbea9fb1a0c17637b572eca7997
nhjoy <32105827+nhjoy@users.noreply.github.com>
2020-08-30 16:35:50 +0800

    mario less added

---
+++ PS 1/mario_less.c
@@ -0,0 +1,25 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height;
+
+    do {
+        height = get_int("Height: ");
+        // user input
+    }
+    while (height < 1 || height > 8);
+
+    for (int i=0; i<height; i++)
+    {
+        for(int j=0; j<height; j++)
+        {
+            if (i+j < height -1)
+                printf(" ");
+            else
+                printf("#");
+        }
+        printf("\n");
+    }
+}
